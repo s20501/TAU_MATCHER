b994d76c34b7014f6cdea5f41ecfa75659d22bde
s20501 <s20501@pjwstk.edu.pl>
2022-11-30 14:03:49 +0100

    Zad 2 Hamcrect

--- zadania/app/src/test/java/zad2/FriendshipsTestsHamcrest.java
+++ zadania/app/src/test/java/zad2/FriendshipsTestsHamcrest.java
@@ -1,4 +1,53 @@
 package zad2;
 
+import org.junit.jupiter.api.Test;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class FriendshipsTestsHamcrest {
+
+    @Test
+    public void testListLength() {
+
+        Friendships friendships = new Friendships();
+        friendships.makeFriends("Marek", "Ferdek");
+        friendships.makeFriends("Marek", "Ewa");
+        friendships.makeFriends("Marek", "Jonny");
+        friendships.makeFriends("Marek", "Krzysiu");
+
+        assertThat(friendships.getFriendsList("Marek"), hasSize(4));
+    }
+
+    @Test
+    public void testListFriends() {
+        Friendships friendships = new Friendships();
+
+        friendships.makeFriends("Marek", "Ferdek");
+        friendships.makeFriends("Marek", "Ewa");
+        friendships.makeFriends("Marek", "Jonny");
+        friendships.makeFriends("Marek", "Krzysiu");
+        assertThat(friendships.getFriendsList("Marek"), contains("Ferdek", "Ewa", "Jonny", "Krzysiu"));
+    }
+
+    @Test
+    public void testListFriendsOrdered() {
+        Friendships friendships = new Friendships();
+
+        friendships.makeFriends("Marek", "Ferdek");
+        friendships.makeFriends("Marek", "Ewa");
+        friendships.makeFriends("Marek", "Jonny");
+        friendships.makeFriends("Marek", "Krzysiu");
+        assertThat(friendships.getFriendsList("Marek"), containsInAnyOrder("Ewa", "Ferdek", "Krzysiu", "Jonny"));
+    }
+
+    @Test
+    public void testListContentLength() {
+        Friendships friendships = new Friendships();
+
+        friendships.makeFriends("Mark", "123");
+        assertThat(friendships.getFriendsList("Mark").size() == 1, equalTo(true));
+    }
+
 }
