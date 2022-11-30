6c60f02d23e2ce1918f0ffe1fdf0691e50803b8e
s20501 <s20501@pjwstk.edu.pl>
2022-11-30 13:53:08 +0100

    Gradle

--- zadania/app/src/main/java/zad2/Friendships.java
+++ zadania/app/src/main/java/zad2/Friendships.java
@@ -1,4 +1,38 @@
 package zad2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Friendships {
+    Map<String, List<String>> friendships = new HashMap<>();
+
+    //Dodanie przyjaciół - wykorzystuje funkcje addFriend!
+    public void makeFriends(String person1, String person2) {
+        addFriend(person1, person2);
+        addFriend(person2, person1);
+    }
+
+    //Pobranie listy przyjaciol
+    public List<String> getFriendsList(String person) {
+        return friendships.get(person);
+    }
+
+    //Sprawdzenie czy przyjaciele
+    public boolean areFriends(String person1, String person2) {
+        return getFriendsList(person1).contains(person2);
+    }
+
+    //Dodanie do listy przyjaciol do danej osoby
+    private void addFriend(String person, String friend) {
+
+        try {
+            getFriendsList(person).add(friend);
+        } catch (Exception e) {
+            friendships.put(person, new ArrayList<>());
+            addFriend(person, friend);
+        }
+
+    }
 }
--- zadania/app/src/test/java/zad2/FriendshipsTestsGradle.java
+++ zadania/app/src/test/java/zad2/FriendshipsTestsGradle.java
@@ -1,4 +1,47 @@
 package zad2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FriendshipsTestsGradle {
+
+
+    @Test
+    public void testMakeFriends() {
+        Friendships friends = new Friendships();
+
+        friends.makeFriends("Adam", "John");
+        assertTrue(friends.areFriends("Adam", "John"));
+    }
+
+    @Test
+    public void testMakeFriendsReverse() {
+        Friendships friends = new Friendships();
+
+        friends.makeFriends("Adam", "John");
+        assertTrue(friends.areFriends("John", "Adam"));
+    }
+
+
+    @Test
+    public void testListNotEmpty() {
+        Friendships friends = new Friendships();
+        friends.makeFriends("Mark", "Adam");
+        Assertions.assertNotEquals(Collections.EMPTY_LIST, friends.getFriendsList("Adam"));
+
+    }
+
+    @Test
+    public void testFriendsListSize() {
+        Friendships friends = new Friendships();
+        friends.makeFriends("Mike", "123");
+        friends.makeFriends("Mike", "124");
+
+        assertTrue(friends.getFriendsList("Mike").size() == 2);
+    }
+
 }
